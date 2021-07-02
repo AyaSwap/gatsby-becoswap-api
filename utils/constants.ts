37ebeae44f5174cfe208c14b81a6086026d8ddcf
AyaSwap <86141106+AyaSwap@users.noreply.github.com>
2021-07-02 21:39:26 +0700

    Update constants.ts

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,15 +1,15 @@
 import { ChainId, Token } from "@becoswap-libs/sdk";
 
 // BEP-20 addresses.
-export const CAKE = "0x8fe4D28476cDd43D36a12EB47dC3243C1925f263";
+export const CAKE = "0x648259243f8a060c38b20dc16214cf0f928f49f1";
 export const WBNB = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c";
 export const DEAD = "0x000000000000000000000000000000000000dEaD";
 
 // Contract addresses.
-export const CAKE_BNB_FARM = "0x93bA7cA08fcB20d0a3381052B8c24bdFb0a9fec7";
-export const MASTERCHEF_CONTRACT = "0x2aE8E3166fF75aF8aBB9bea2D453F084cBcd8FDA";
+export const CAKE_BNB_FARM = "0x704923893Eb29E989bB34f95cE57aC906613d907";
+export const MASTERCHEF_CONTRACT = "0xE30D1F5E94C8e45d473FCA48e66136D719D5401c";
 export const LOTTERY_CONTRACT = "0x091fF4ee8b12dF117a9b3A9F3D7E8fd5f6F79ac1";
-export const MULTICALL_CONTRACT = "0x1Ee38d535d541c55C9dae27B12edf090C608E6Fb";
+export const MULTICALL_CONTRACT = "0xa599b2591d4b5b5d3b3b158efa9bd155b0107b1f";
 
 // PancakeSwap SDK Token.
 export const CAKE_TOKEN = new Token(ChainId.MAINNET, CAKE, 18);
